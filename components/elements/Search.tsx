d4fb3be44a4734e36b19296280ccfba60f11350b
dachieng <oloodorcas99@gmail.com>
2023-06-07 19:29:02 +0300

    refractor: search -> async paginate

--- components/elements/Search.tsx
+++ components/elements/Search.tsx
@@ -4,18 +4,61 @@ import React, { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
 
 interface Props {
-  data: any;
+  handleOnSearchChange: (data: any) => void;
 }
 
-const Search: React.FC<Props> = ({ data }) => {
+const Search: React.FC<Props> = ({ handleOnSearchChange }) => {
+  const [search, setSearch] = useState<any>(null);
+
+  const url = process.env.NEXT_PUBLIC_CITIES_API_URL;
+  const key = process.env.NEXT_PUBLIC_XRAPID_API_KEY as string;
+  const host = process.env.NEXT_PUBLIC_XRAPID_API_HOST as string;
+
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": key,
+      "X-RapidAPI-Host": host,
+    },
+  };
+
+  const handleChange = (opt: any) => {
+    setSearch(opt);
+    handleOnSearchChange(opt);
+  };
+
+  const loadOptions = async (inputValue: string) => {
+    try {
+      const res = await fetch(
+        `${url}/cities?namePrefix=${inputValue}`,
+        options
+      );
+      const data = await res.json();
+
+      console.log("data", data.data);
+
+      const dataValues = data.data.map((city: any) => ({
+        label: `${city.countryCode} - ${city.name}`,
+        value: `${city.latitude} ${city.longitude}`,
+      }));
+
+      return {
+        options: dataValues,
+      };
+    } catch (error: any) {
+      console.log("error", error);
+    }
+  };
+
   return (
     <div className='w-full md:max-w-[67.5rem] my-[0.5rem] md:my-[2rem] mx-auto px-[0.5rem] relative'>
-      {/* <AsyncPaginate
+      <AsyncPaginate
+        // @ts-expect-error
+        loadOptions={loadOptions}
         value={search}
-        placeholder='Search location'
+        onChange={handleChange}
         debounceTimeout={600}
-      /> */}
-      This is the search component
+      />
     </div>
   );
 };
--- modules/weather/elements/index.tsx
+++ modules/weather/elements/index.tsx
@@ -1,22 +1,35 @@
 "use client";
 
-import { fetchCities } from "@/redux/cities/actionCreators";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+
+import Search from "@/components/elements/Search";
+import { fetchCities } from "@/redux/cities/actionCreators";
 
 interface Props {}
 
 const WeatherModule: React.FC<Props> = () => {
+  const [search, setSearch] = useState("");
+
   const cities = useSelector((state: any) => state.cities);
   const dispatch = useDispatch();
+
+  const handleOnSearchChange = (data: any) => {
+    console.log("data", data);
+  };
+
+  console.log("search", search);
 
   useEffect(() => {
     //@ts-expect-error Argument of type '(dispatch: any) => Promise<void>' is not assignable to parameter of type 'AnyAction'
     dispatch(fetchCities(""));
   }, []);
 
-  console.log("cities", cities);
-  return <div className='min-h-screen'>kj</div>;
+  return (
+    <div className='min-h-screen'>
+      <Search handleOnSearchChange={handleOnSearchChange} />
+    </div>
+  );
 };
 
 export default WeatherModule;
